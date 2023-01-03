f5c9ba0084b4d60cce08d13f4f8d31525fcef0a3
HXLike <1936400036@qq.com>
2023-01-03 18:38:51 +0800

    ssm配置文件基本完成

--- src/main/java/huang/xu/config/MybatisConfig.java
+++ src/main/java/huang/xu/config/MybatisConfig.java
@@ -1,4 +1,23 @@
 package huang.xu.config;
 
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
 public class MybatisConfig {
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setTypeAliasesPackage("huang.xu.domain");
+        return factoryBean;
+    }
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer() {
+        MapperScannerConfigurer msc = new MapperScannerConfigurer();
+        msc.setBasePackage("huang.xu.dao");
+        return msc;
+    }
 }
---
+++ src/main/java/huang/xu/config/SpringMvcConfig.java
@@ -0,0 +1,11 @@
+package huang.xu.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan({"huang.xu.config"})
+@EnableWebMvc
+public class SpringMvcConfig {
+}
